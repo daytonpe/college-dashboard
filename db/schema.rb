00ebfcf64778c3c234391d806e3b15a79131d303
Pat Dayton <daytonpe14@gmail.com>
2016-08-17 17:40:30 -0500

    Update college model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816164251) do
+ActiveRecord::Schema.define(version: 20160817223952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "colleges", force: :cascade do |t|
+    t.string   "name"
+    t.string   "web_page"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
