2a3481fa740146763057c2ff6e8863fc566948fb
Pat Dayton <daytonpe14@gmail.com>
2016-08-28 13:20:04 -0500

    Add counter

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,14 +1,17 @@
 class ChecklistsController < ApplicationController
   def toggle
-  	@checklist = Checklist.find(params[:id])
-  	@checklist[params[:attr]] = !@checklist[params[:attr]]
+    @checklist = Checklist.find(params[:id])
+    @checklist[params[:attr]] = !@checklist[params[:attr]]
+
+    # Adjust the counter
+    if @checklist[params[:attr]]
+      @checklist.count+=1
+    else
+      @checklist.count-=1
+    end
 
   	@checklist.save
   	redirect_to :back
-  	# if @checklist.c1
-   #    @checklist.count++
-   #  else
-   #    @checklist.count--
-   #  end
+
   end
 end
